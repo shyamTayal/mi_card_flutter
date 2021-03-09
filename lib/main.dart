aba6198d8bc1b725c9a634fbdd0335dc2c38034a
Shyam Tayal <shyam.tayal@hotmail.com>
2021-03-09 12:38:03 +0530

    Making Contact Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.red,
+                  foregroundImage: AssetImage('images/Kratos.jpg')
+                ),
+                Container(
+                  child: Text(
+                    'Kratos Eros',
+                    textAlign: TextAlign.center,
+                  ),
+                  height: 40.0,
+                  width:250.0
+                )
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
+
